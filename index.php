301b41e9839d103dbef8bc1913b92cf922fdef30
Fernando Medina <52418740+FERNANDO1008@users.noreply.github.com>
2021-08-18 22:54:18 -0500

    final del proyecto
    terminado

---
+++ index.php
@@ -0,0 +1,95 @@
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ChatBot with PHP</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/custom.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="node_modules/open-iconic/font/css/open-iconic-bootstrap.min.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>	
+	<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" >
+
+<script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
+
+
+</head>
+
+<body>
+<div class="container">
+	
+	
+</div>
+   
+   <div class="chatbo">
+  
+<df-messenger
+  intent="WELCOME"
+  chat-title="CORONABOOT"
+  agent-id="7ecdd07c-8722-44c8-bf83-55031cf97453"
+  language-code="es"
+></df-messenger>
+   </div>
+    
+    <script src="app.js"></script>
+    
+            <SCRIPT LANGUAGE="JavaScript">
+        function mi_alerta () {
+        alert ("Proyecto UNL"+
+               "\n"+
+               "\nluis.f.medina@unl.edu.ec");
+        }
+        </SCRIPT>
+        
+</body>
+
+<style type="text/css">
+.footer {
+    background-image: url(http://www.keoniworld.com/wp-content/uploads/2016/06/swaying-field-grass.gif);
+    background-position: center bottom;
+    background-size: 100% auto;
+    background-repeat: repeat-x;
+    min-height: 10vh;
+}
+
+.footer {
+    text-align: center;
+    padding: 50px 0 30px;
+}
+
+.footer p {
+    color: #f8f8f8;
+    font-size: 24px;
+}
+
+.footer a.totop {
+    color: #fff;
+    margin-bottom: 30px;
+}
+
+.footer a.totop:focus {
+    outline: 0;
+}
+
+</style>
+<footer class="footer">
+    <nav>
+            <p>
+                <a href="acerca.html">Contacto: 0939045202</a>|
+                <a href="https://www.geeksforgeeks.org/privacy-policy/">luis.f.medina@unl.edu.ec</a>|
+                <a href="https://www.geeksforgeeks.org/careers/">2020</a>
+                <p>&copy; Copyright 2020 HTML.am</p>
+                <button onclick="location.href='index.php'" class="submit" type="button">INICIO</button>
+            </p>
+        </nav>
+    </footer>
+<script type="text/javascript " src="node_modules/popper.js/dist/umd/popper.min.js "></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js " integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj " crossorigin="anonymous "></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js " integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx " crossorigin="anonymous "></script>
+
+</html>
